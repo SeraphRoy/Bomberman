a30306ebfd909b1d9ff152ec78b38d8e7a8373c7
Shaoyi Zhang <zhangshaoyi1995@gmail.com>
2016-01-24 17:31:28 -0800

    Bomb now can trigger nearby bombs

--- Bomb.py
+++ Bomb.py
@@ -5,20 +5,19 @@ from Img import transparent_image
 class Bomb:
 
 
-    def __init__(self,image, x = -50, y = -50,length = 2):
+    def __init__(self,image, x = -50, y = -50,damageLength = 1, isNull = False):
         BOMB_IMG_X = 50
         BOMB_IMG_Y = 50
         temp = pygame.image.load(image).convert_alpha()
+        self.damageLength = 1
         self.image = pygame.transform.scale(temp,(BOMB_IMG_X,BOMB_IMG_Y))
         self.time = 2.0
         self.x_Index = int(x//BOMB_IMG_X)
         self.x = self.x_Index * BOMB_IMG_X
         self.y_Index = int(y//BOMB_IMG_Y)
         self.y = self.y_Index * BOMB_IMG_Y
-
-        self.length = length
-
-
+        self.isNull = isNull
+        
 
     def TimePassed(self,t):
         self.time-=t
@@ -36,6 +35,16 @@ class Bomb:
     def SetY(self,y):
         self.y = y
 
+    def SetDamageLength(self,damageLength):
+        self.damageLength = damageLength
+
+    # Call this method when player recieved an increase-bomb-power item
+    def IncrDamageLength(self):             
+        self.damageLength += 1
+
+    def GetDamageLength(self):
+        return self.damageLength
+
     def GetX(self):
         return self.x
     
@@ -43,25 +52,45 @@ class Bomb:
         return self.y
 
     def GetX_Index(self):
-        #print self.x_Index
-        #print self.x
         return self.x_Index
 
     def GetY_Index(self):
-        #print self.y_Index
-        #print self.y
         return self.y_Index
+
+    def IsNull(self):
+        return self.isNull
+
+
 
 class BombMatrix:
 
     def __init__(self,X_MAX=13,Y_MAX=15):
         self.X_MAX = X_MAX
         self.Y_MAX = Y_MAX
-        self.null_Bomb = Bomb(transparent_image,-50,-50)
+        self.null_Bomb = Bomb(transparent_image,-50,-50,0,True)
         self.bombMatrix = [[self.null_Bomb for x in range(X_MAX)] for y in range(Y_MAX)]
 
     def AddBomb(self, newBomb):
         self.bombMatrix[newBomb.GetX_Index()][newBomb.GetY_Index()] = newBomb
 
-    def RemoveBomb(self, x_Index, y_Index):
+    def RemoveBomb(self,screen, x_Index, y_Index,burst):
+        screen.blit(burst,(self.bombMatrix[y_Index][x_Index].GetX(),self.bombMatrix[y_Index][x_Index].GetY()))
         self.bombMatrix[y_Index][x_Index] = self.null_Bomb
+
+        if (x_Index >= 1) and (self.bombMatrix[y_Index][x_Index-1].IsNull() == False):
+            self.RemoveBomb(screen,x_Index-1,y_Index,burst)
+        if (y_Index >= 1) and (self.bombMatrix[y_Index-1][x_Index].IsNull() == False):
+            self.RemoveBomb(screen,x_Index,y_Index-1,burst)
+        if (x_Index <= self.X_MAX-2) and (self.bombMatrix[y_Index][x_Index+1].IsNull() == False):
+            self.RemoveBomb(screen,x_Index+1,y_Index,burst)
+        if (y_Index <= self.Y_MAX-2) and (self.bombMatrix[y_Index+1][x_Index].IsNull() == False):
+            self.RemoveBomb(screen,x_Index,y_Index+1,burst)
+
+    def CheckAllBombs(self, screen, current_time, X_INDEX, Y_INDEX,burst):
+        for x in range(X_INDEX):
+            for y in range(Y_INDEX):
+                if (self.bombMatrix[y][x].isNull == False) and (self.bombMatrix[y][x].TimePassed(current_time) == True):
+                    self.RemoveBomb(screen,x,y,burst)
+                else:
+                   screen.blit(self.bombMatrix[y][x].GetImage(),(self.bombMatrix[y][x].GetX(),self.bombMatrix[y][x].GetY()))
+
